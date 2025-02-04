3aad0f7bdac26f3243ed74b7fb474d7090099693
Gleb Fedoseev <heartplay.gf@gmail.com>
2025-02-04 14:08:34 +0500

    Add switching players

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
 // Selecting elements
+const player1Elem = document.querySelector(`.player--0`);
 const score1Elem = document.querySelector(`#score--0`);
 const curScore1Elem = document.getElementById(`current--0`);
+
+const player2Elem = document.querySelector(`.player--1`);
 const score2Elem = document.getElementById(`score--1`);
 const curScore2Elem = document.getElementById(`current--1`);
+
 const diceElem = document.querySelector(`.dice`);
 const btnRollDice = document.querySelector(`.btn--roll`);
 const btnNewGame = document.querySelector(`.btn--new`);
 const btnHoldScore = document.querySelector(`.btn--hold`);
 
 // Starting conditions
+const totalScores = [0, 0];
 score1Elem.textContent = 0;
 score2Elem.textContent = 0;
 diceElem.classList.add(`hidden`);
 let currentScore = 0;
+let currentPlayer = 0;
 
 // Dice roll
 btnRollDice.addEventListener(`click`, function () {
@@ -21,18 +27,28 @@ btnRollDice.addEventListener(`click`, function () {
     const diceRoll = Math.trunc(Math.random() * 6) + 1;
     console.log(diceRoll);
 
-    // Display dice in accordance with dice roll
+    // Display dice according to dice roll
     diceElem.classList.remove(`hidden`);
     diceElem.src = `dice-${diceRoll}.png`;
 
     // Check roll
     if (diceRoll !== 1) {
-        // Add dice roll to score
+        // Add dice roll to current score
         currentScore += diceRoll;
-        curScore1Elem.textContent = currentScore;
+        activePlayer(currentPlayer).textContent = currentScore;
     } else {
-        // Switch player
+        // Switch player if dice roll is 1 and reset current score
         currentScore = 0;
-        curScore1Elem.textContent = currentScore;
+        activePlayer(currentPlayer).textContent = currentScore;
+
+        currentPlayer = currentPlayer == 0 ? 1 : 0;
+        player1Elem.classList.toggle(`player--active`);
+        player2Elem.classList.toggle(`player--active`);
     }
 });
+
+btnHoldScore.addEventListener(`click`, function () {});
+
+const activePlayer = function (currentPlayer) {
+    return document.getElementById(`current--${currentPlayer}`);
+};
